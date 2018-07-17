17f866fdb98aae76e19a949fcb114be5e8bf6b9c
Abhi Jani <abhijani@Abhis-MacBook-Pro.local>
2018-07-17 16:13:40 -0500

    first commit

---
+++ TikToc/GeneralMode.swift
@@ -0,0 +1,13 @@
+//
+//  GeneralMode.swift
+//  TikToc
+//
+//  Created by Abhi Jani on 7/16/18.
+//  Copyright © 2018 Abhi Jani. All rights reserved.
+//
+
+import UIKit
+
+class GeneralMode: NSObject {
+
+}
